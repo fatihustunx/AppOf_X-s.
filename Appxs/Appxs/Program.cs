77b2f382ce9ab78a28d52c82493ed070ad9ba066
Wistaster <wistaster@hotmail.com>
2024-01-31 23:17:21 +0300

    App

---
+++ Appxs/Appxs/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Appxs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, Worlds!");
+
+        }
+
+    }
+}
